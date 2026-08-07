8a0ec297026c19fb95bc055cdeedaf96d8241c53
Fifenya <mn5536348@gmail.com>
2026-08-07 14:46:06 +0600

    Create MainActivity.kt

---
+++ client/android/app/src/main/java/com/nexus/messenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nexus.messenger
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+  override fun getMainComponentName() = "nexusmobile"
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
