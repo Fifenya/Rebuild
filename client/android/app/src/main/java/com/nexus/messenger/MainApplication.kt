47ad0e8ab73a199db2e171782010ca56b347b473
Fifenya <mn5536348@gmail.com>
2026-08-07 18:24:56 +0600

    Update MainApplication.kt

--- client/android/app/src/main/java/com/nexus/messenger/MainApplication.kt
+++ client/android/app/src/main/java/com/nexus/messenger/MainApplication.kt
@@ -1,4 +1,5 @@
 package com.nexus.messenger
+
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
@@ -7,21 +8,35 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
+
   override val reactNativeHost: ReactNativeHost =
-      object : ReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = PackageList(this).packages.apply {}
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).apply {
+              // Пакеты, которые нельзя автолинкнуть, добавляются вручную:
+              add(IconSwitcherPackage())
+            }.packages
+
         override fun getJSMainModuleName(): String = "index"
+
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-  override val reactHost: ReactHost get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) load()
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      load()
+    }
   }
 }
